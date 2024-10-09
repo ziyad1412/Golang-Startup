1f04c82c8c59d127df1fea4003e7affde0dd450f
ziyad1412 <abdurrahman.ziyad17@gmail.com>
2024-10-09 11:59:59 +0700

    Login User Api

--- handler/user.go
+++ handler/user.go
@@ -49,3 +49,35 @@ func (h *userHandler) RegisterUser(c *gin.Context) {
 
 	c.JSON(200, response)
 }
+
+func (h *userHandler) Login(c *gin.Context) {
+
+	var input user.LoginInput
+
+	err := c.ShouldBindJSON(&input)
+
+	if err != nil {
+		errors := helper.FormatValidationError(err)
+		errorMessage := gin.H{"errors": errors}
+
+		response := helper.APIResponse("Login failed", 422, "error", errorMessage)
+		c.JSON(422, response)
+		return
+	}
+
+	loggedInUser, err := h.userService.Login(input)
+
+	if err != nil {
+		errorMessage := gin.H{"errors": err.Error()}
+
+		response := helper.APIResponse("Login failed", 422, "error", errorMessage)
+		c.JSON(422, response)
+		return
+	}
+
+	formatter := user.FormatUser(loggedInUser, "token")
+
+	response := helper.APIResponse("Successfuly logged in", 200, "success", formatter)
+
+	c.JSON(200, response)
+}
